20d74d53ef56ffe50995caf2028e7111acbdee6c
amhidalgo <ahidalgo@verizon.net>
2016-02-07 15:17:52 -0600

    final static todos

--- server.js
+++ server.js
@@ -26,10 +26,18 @@ app.get('/todos',function(req, res) {
 
     } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false')
     {
-        filteredTodos = _.where(filteredTodos,{completed: false});        
+        filteredTodos = _.where(filteredTodos,{completed: false});   
     }
-    //else {filteredTodos = [];}
-    //res.send(queryParams);
+
+    // GET /todos?completed=false&q=home
+    // "got o work on saturday".indexof("work") -1 = false else index
+    
+    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0)
+    {
+        filteredTodos = _.filter(filteredTodos, function(todo) {
+            return todo.description.toLowerCase().indexOf(queryParams.q.toLocaleLowerCase()) > -1;
+        });
+    }
     
     res.json(filteredTodos);
 });
