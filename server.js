0e5e2f42dd808a0ce6751b8db7d414bcd6c6221e
amhidalgo <ahidalgo@verizon.net>
2016-02-07 01:26:27 -0600

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -33,30 +33,59 @@ app.get('/todos/:id', function(req, res) {
 });
 
 app.post('/todos', function(req, res) {
-    //var body = req.body;
-    var body = _.pick(req.body,'description', 'completed')
+    var body = _.pick(req.body, 'description', 'completed');
     
     if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
         return res.status(400).send();
     }
-    body.id = todoNextId++;
+
     body.description = body.description.trim();
+    body.id = todoNextId++;
+    
     todos.push(body);
-    //console.log('description: ' + body);
+
     res.json(body);
-})
+});
 
 app.delete('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo = _.findWhere(todos,{id: todoId});
-    if (matchedTodo) {
+    if (!matchedTodo) {
+        res.status(404).json({"error": "no todo found with that Id"});
+    } else {
         todos = _.without(todos, matchedTodo);
         res.json(matchedTodo);
-    } else {
-        res.status(404).json({"error": "no todo found with that Id"});
     }
 });
-    //res.send('Asking for todo with id of: ' + req.params.id);
+
+// put - to update todo item
+app.put('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos,{id: todoId});    
+    var body = _.pick(req.body,'description', 'completed');
+    var validAttributes = {};
+    
+    if (!matchedTodo) {
+        return res.status(400).send();
+    }
+    
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if(body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    } 
+    
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0)
+    {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+    
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
+    
+});
 
 app.listen(PORT, function() {
     console.log('Express listening on port: ' + PORT);
