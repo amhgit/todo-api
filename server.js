53dc8b5e79660b48b6218b55190abd482572cbc6
amhidalgo <ahidalgo@verizon.net>
2016-02-08 00:20:48 -0600

    Convert GET /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -46,28 +46,21 @@ app.get('/todos',function(req, res) {
 // get one todo by id
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos,{id: todoId});
     
-    if (matchedTodo) {
-        res.json(matchedTodo);
-    } else {
-        res.status(404).send();
-    }
+    db.todo.findById(todoId).then(function(todo) {
+        if (!!todo) {
+           res.json(todo.toJSON()); 
+        } else {
+            res.status(404).send();
+        }       
+    }, function(e) {
+        res.status(500).send();
+    });
 });
 
 app.post('/todos', function(req, res) {
     var body = _.pick(req.body, 'description', 'completed');
     
-//    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-//        return res.status(400).send();
-//    }
-//
-//    body.description = body.description.trim();
-//    body.id = todoNextId++;
-//    
-//    todos.push(body);
-//
-//    res.json(body);
     db.todo.create(body).then(function(todo) {
        res.json(todo.toJSON());
     }, function(e) {
@@ -116,9 +109,8 @@ app.put('/todos/:id', function(req, res) {
 });
 
 db.sequelize.sync().then(function() {
-app.listen(PORT, function() {
-    console.log('Express listening on port: ' + PORT);
-});
-    
+    app.listen(PORT, function() {
+        console.log('Express listening on port: ' + PORT);
+    });    
 });
 
