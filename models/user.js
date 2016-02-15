15942e389aba1b8e51eb28eb8577d82c84d0262b
amhidalgo <ahidalgo@verizon.net>
2016-02-15 01:17:17 -0600

    Add auth token generation to POSt /users/login

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken'); 
 
 module.exports = function(sequelize, DataTypes) {
     var user = sequelize.define('user',{        
@@ -68,6 +70,27 @@ module.exports = function(sequelize, DataTypes) {
                 var json = this.toJSON(); 
                 return _.pick(json,'id', 'email', 'updatedAt', 'createdAt');
             }
+        },
+        geberateToken: function(type) {
+          if (!_,isString(type)) {
+            return undefined;
+          }
+          try {
+            //install new npm module for user token jasonwebtoken
+            // install crypto-js03.1.5 --save
+            var stringData = JSON.stringify({id: thig.get('id'), type: type});
+            var enryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#!').toString();
+            var token = jwt.sign({
+              token: encryptedData
+            }, 'qwerty098');
+            return token;
+
+          } catch(e) {
+            console.error(e);
+            return undefined;
+
+          }
+
         }
     });
     return user;
